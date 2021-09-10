130475f5f44dad2dab1ed4859f25312d15f88a6e
Robert Andreev <andreev.robert.a@gmail.com>
2021-09-10 14:21:33 +0300

    Add constant

---
+++ RESTAssured/src/main/java/com/epam/constants/Authorization.java
@@ -0,0 +1,6 @@
+package com.epam.constants;
+
+public class Authorization {
+    public static final String KEY = "key";
+    public static final String TOKEN = "token";
+}
---
+++ RESTAssured/src/main/java/com/epam/constants/BoardsParametersName.java
@@ -0,0 +1,6 @@
+package com.epam.constants;
+
+public class BoardsParametersName {
+    public static final String NAME = "name";
+    public static final String DESC = "desc";
+}
