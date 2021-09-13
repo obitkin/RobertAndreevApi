549b0a2b3706570ed62083640e4866a44d465242
Robert Andreev <andreev.robert.a@gmail.com>
2021-09-13 15:27:10 +0300

    Add constant

--- RESTAssured/src/main/java/com/epam/constants/BoardsParametersName.java
+++ RESTAssured/src/main/java/com/epam/constants/BoardsParametersName.java
@@ -3,4 +3,7 @@ package com.epam.constants;
 public class BoardsParametersName {
     public static final String NAME = "name";
     public static final String DESC = "desc";
+    public static final String ID = "id";
+    public static final String CLOSED = "closed";
+    public static final String DEFAULT_LISTS = "defaultLists";
 }
